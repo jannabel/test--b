03f85e5c810c2c1c748917e579df9f1bedc5b213
jannabelramos <jannabelramos@gmail.com>
2022-04-18 21:05:36 -0400

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,68 @@
-import 'package:biometric_test/home.dart';
 import 'package:flutter/material.dart';
 import 'package:local_auth/local_auth.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:nb_utils/nb_utils.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _LoginPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
+class _MyHomePageState extends State < MyHomePage > {
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+  String _message = "Not Authorized";
 
-class _LoginPageState extends State<MyHomePage> {
-  late LocalAuthentication _localAth;
-  bool _isBiometricAvailable = false;
+  Future < bool > checkingForBioMetrics() async {
+    bool canCheckBiometrics = await _localAuthentication.canCheckBiometrics;
+    print(canCheckBiometrics);
+    return canCheckBiometrics;
+  }
+  Future < void > _authenticateMe() async {
+    // 8. this method opens a dialog for fingerprint authentication.
+    //    we do not need to create a dialog nut it popsup from device natively.
+    bool authenticated = false;
+    try {
+      authenticated = await _localAuthentication.authenticateWithBiometrics(
+        localizedReason: "Authenticate for Testing", // message for dialog
+        useErrorDialogs: true, // show error in dialog
+        stickyAuth: true, // native process
+      );
+      setState(() {
+        _message = authenticated ? "Authorized" : "Not Authorized";
+      });
+    } catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+  }
 
   @override
   void initState() {
+    // TODO: implement initState
+    checkingForBioMetrics();
     super.initState();
-    _localAth = LocalAuthentication();
-    _localAth.canCheckBiometrics.then((b) {
-      setState(() {
-        _isBiometricAvailable = b;
-      });
-    });
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Container(
+        child: Text("$_message"),
       ),
-      body: SizedBox(
-        height: 45,
-        width: double.infinity,
-        child: ElevatedButton(
-          onPressed: () async {
-            bool didAuthenticate = await _localAth.authenticate(
-                localizedReason: "Iniciar sesion con huella");
-            if (didAuthenticate) {
-              TestScreen().launch(context,
-                  isNewTask: true,
-                  pageRouteAnimation: PageRouteAnimation.Slide);
-            }
-          },
-          style: ElevatedButton.styleFrom(
-            elevation: 0,
-            primary: Colors.amber,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),
-          child: const Text('Iniciar Sesión',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold)),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _authenticateMe,
+        child: Icon(Icons.add),
       ),
     );
   }
