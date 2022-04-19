352ae1a20661b49423d533f0223925dc71bbe599
jannabelramos <jannabelramos@gmail.com>
2022-04-18 20:17:25 -0400

    all

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatefulWidget {
+  @override
+  _TestScreenState createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('COOPDGII'),
+      ),
+      body: Column(children: <Widget>[]),
+    );
+  }
+}
